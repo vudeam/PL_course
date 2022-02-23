a5a36b0e8942886e33d4944ecd68dbd7a3dc777a
Vadim Ermakov <vadim22.barnaul@gmail.com>
2022-02-23 23:46:46 +0300

    Add menu defines

--- include/stuff.hpp
+++ include/stuff.hpp
@@ -17,6 +17,16 @@
 #define STR_PATH      0x08
 #define STR_SUBJ      0x0A
 
+#define MENUTYPE_ARROWS    0x01
+#define MENUTYPE_KEYS      0x02
+
+#define KEY_LOWER_W       'w'
+#define KEY_LOWER_S       's'
+#define KEY_ARROW_UP      72
+#define KEY_ARROW_DOWN    80
+#define ASCII_LF          0x0A
+#define ASCII_CR          0x0D
+
 
 std::string
 GetValidString (int option = STR_ENG | STR_RUS | STR_DIGITS | STR_PATH | STR_SUBJ);
